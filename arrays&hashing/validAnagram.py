db348444a4e935093a0384ac40e027a03a44e4eb
TuckerDay <tucker.d.day@gmail.com>
2022-04-22 21:29:09 -0600

    Clarified and extended comments

--- arrays&hashing/validAnagram.py
+++ arrays&hashing/validAnagram.py
@@ -8,9 +8,12 @@ class Solution(object):
         :rtype: bool
         """
 
+        # If the strings are different lengths they cannot be annagrams
         if len(s) != len(t):
             return False
 
+        # Create dictionaries for each word with the key as a letter and the
+        # value as the number of times the letter occurs
         dict_s = {}
         dict_t = {}
 
@@ -20,6 +23,7 @@ class Solution(object):
         for ch in t:
             dict_t[ch] = dict_t.get(ch, 0) + 1
 
+        # Go through the keys in dict_s and see if dict_t has the same value for each key
         for key in dict_s:
             if (not dict_t.has_key(key)) or (dict_s[key] != dict_t[key]):
                 return False
